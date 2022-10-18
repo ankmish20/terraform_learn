05225b95f649f18a11065d29277002523fbd7f4f
Ankit Mishra <ankmish20@gmail.com>
2022-10-18 17:56:03 -0400

    new update

--- main.tf
+++ main.tf
@@ -4,6 +4,30 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+
+resource "google_compute_network" "vpc_network" {
+  project                 = "voltaic-azimuth-365215"
+  name                    = "vpc-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_vpn_gateway" "igw" {
+  name    = "vpn-1"
+  network = google_compute_network.vpc_network.id
+}
+
+
+resource "google_compute_subnetwork" "subnet1" {
+  name          = "subnet1"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+
+}
+
+
+
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "e2-micro"
@@ -15,7 +39,7 @@ resource "google_compute_instance" "vm_instance" {
   }
   network_interface {
     # A default network is created for all GCP projects
-    network = "default"
+    subnetwork = google_compute_subnetwork.subnet1.id
     access_config {
     }
   }
